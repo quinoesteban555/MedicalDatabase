91fbb5fe32d29577b25bd80bd58f61cc6c99b0a9
quinoesteban555 <69019687+quinoesteban555@users.noreply.github.com>
2022-10-31 00:42:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,26 @@ $port = $_SERVER['WEBSITE_MYSQL_PORT'];
 $servername = "localhost:$port";
 $username = "azure";
 $password = "6#vWHD_$";
+$db = "localdb";
 
+$mysqli = new mysqli($servername,$username,$password,$db);
 // Create connection
-$conn = new mysqli($servername, $username, $password);
 
 // Check connection
-if ($conn->connect_error) {
+if ($mysqli->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-?> 
+
+$sql = "SELECT message FROM helloworld";
+$result = $mysqli->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "message: " . $row["message"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$mysqli->close();
+?>
